33b828e904c7047b098707e3b4cdd15f4c089ffe
Mikhail Zinchik <MVMGroup888@yandex.ru>
2024-02-24 10:53:30 +0300

    add preloader

--- src/components/SalonsFolder/Salons/Salons.js
+++ src/components/SalonsFolder/Salons/Salons.js
@@ -5,14 +5,15 @@ import { Link } from "react-router-dom";
 import Api from '../../../utils/Api';
 import { CurrentUserContext } from '../../../context/CurrentUserContext';
 import Button from '../../elements/Button/Button';
+import Preloader from '../../elements/Preloader/Preloader';
 
 const Salons = () => {
   const currentUser = React.useContext(CurrentUserContext);
   const [salons, setSalons] = useState([]);
   const [salonInfo, setSalonInfo] = useState(null);
   const { t } = useTranslation();
-
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingTimeout, setLoadingTimeout] = useState(null);
 
   const api = new Api({
     url: 'http://localhost:5000',
@@ -27,8 +28,16 @@ const Salons = () => {
     }
   }, [currentUser]);
 
+  useEffect(() => {
+    setLoadingTimeout(setTimeout(() => {
+      setIsLoading(false);
+    }, 10000));
+    return () => clearTimeout(loadingTimeout);
+  }, [salons]);
+
 
   function getSalonsByRegionForCurrentUser() {
+    setIsLoading(true);
     return api
       .getAllSalonsInRegion(currentUser.regionId)
       .then((salons) => {
@@ -38,7 +47,10 @@ const Salons = () => {
         });
 
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
 
   async function getSalonDetails(salonId) {
@@ -55,21 +67,24 @@ const Salons = () => {
       <h2 className="salons__header">{t("AllSalons")}</h2>
       <div className="salons__block">
         <div className="salons__container">
-          {salons.length > 0 ? (
-            <ul className="salons__list">
-              {salons.map((salon) => (
-                <li key={salon.salonId} className="salons__item">
-                  <Link to={`/salons/${salon.salonId}`} className="salons__card">
-                    {salon.salonName} {`${salon.salonRate !== null ? salon.salonRate : ''}`}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : <p className="salons__item notfound">Салонов в вашем регионе не найдено</p>}
+          {/* <Preloader /> */}
+          {isLoading ? (<Preloader />) : (
+            salons.length > 0 ? (
+              <ul className="salons__list">
+                {salons.map((salon) => (
+                  <li key={salon.salonId} className="salons__item">
+                    <Link to={`/salons/${salon.salonId}`} className="salons__card">
+                      {salon.salonName} {`${salon.salonRate !== null ? salon.salonRate : ''}`}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : <p className="salons__item notfound">Салонов в вашем регионе не найдено</p>
+          )}
         </div>
         <Button type="button" buttonText={t("Back2")} to="/" />
       </div>
-    </div>
+    </div >
   );
 }
 
---
+++ src/components/elements/Preloader/Preloader.js
@@ -0,0 +1,52 @@
+import "./Preloader.css";
+
+const Preloader = () => {
+  return (
+    <div class="preloader">
+      <div class="preloader__item">
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          </div>
+        </div>
+
+        <div>
+          <div
+          ></div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
